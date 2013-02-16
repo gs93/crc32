5d1b9e107faf154ed5bed07cb92cc50300d70802
Giuliano Schneider <gs93@gmx.net>
2013-02-16 22:25:59 +0100

    initial commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+CXX = g++
+CXXFLAGS = -Wall -Wextra -Weffc++ -pedantic
+RM = rm -f
+CP = cp
+SRC = crc32.cpp
+LDLIBS = -lboost_regex
+DESTDIR = /usr/local
+
+all: crc32
+
+crc32: $(OBJS)
+	g++ $(LDFLAGS) -o crc32 $(SRC) $(LDLIBS) 
+
+install:
+	$(CP) "crc32" "$(DESTDIR)/bin"
+
+uninstall:
+	$(RM) "$(DESTDIR)/bin/crc32"
+
+clean:
+	$(RM) "crc32"
---
+++ crc32.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <boost/regex.hpp>
+#include <boost/crc.hpp>
+
+using namespace std;
+
+const string colorReset     = "\x1b[39m", 
+             colorGreen     = "\x1b[32m",
+             colorRed       = "\x1b[31m",
+             colorMagenta   = "\x1b[35m";
+
+unsigned long calculateCrc32(ifstream &inf)
+{
+    const size_t maxRead = 1048576;
+    char buffer[maxRead];
+    boost::crc_32_type chksum;
+    while (inf) {
+        inf.read(buffer, maxRead);
+        chksum.process_bytes(buffer, inf.gcount());
+    }
+    return chksum.checksum();
+}
+
+unsigned long getCrc32FromFilename(const string &file)
+{
+    boost::regex crcRegex("([\\[\\(]+)([a-fA-F0-9]{8})([\\]\\)]+)");
+    boost::match_results<string::const_iterator> what;
+    if (boost::regex_search(file.begin(), file.end(), what, crcRegex)) {
+        unsigned long crc;
+        // 0: complete match, 1: first match, 2: second match, 3: third match
+        istringstream(what[2]) >> hex >> crc; // convert hex string into dec ulong
+        return crc;
+    }
+    return 0; // nothing found
+}
+
+int main(int argc, const char *argv[])
+{
+    if (argc < 2) {
+        cout << argv[0] << " file [file2 [ ..." << endl;
+        return 1;
+    }
+
+    ifstream inf;
+    bool errorOccurred = false;
+    for (int arg = 1; argv[arg]; arg++) {
+        inf.open(argv[arg], ios::binary);
+        if (inf) {
+            unsigned long calcCrc = calculateCrc32(inf),
+                wantedCrc = getCrc32FromFilename(argv[arg]);
+
+            if (calcCrc == wantedCrc)
+                cout << colorGreen;
+            else if (wantedCrc == 0) // no crc found
+                cout << colorMagenta;
+            else
+                cout << colorRed;
+
+            cout.width(8);
+            cout << hex << calcCrc << colorReset << "   " << argv[arg] << endl;
+        } else {
+            errorOccurred = true;
+            cout << colorRed << "    fail" << colorReset << "   " << argv[arg] << endl;
+        }
+        inf.close();
+    }
+    return errorOccurred;
+}
